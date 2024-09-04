235c9c0c2195a030f07e65f88f97ddcf14508bde
fadimecoşgun <102363800+ABCDSAVC@users.noreply.github.com>
2024-09-04 16:16:53 +0300

    Add files via upload

---
+++ ilkdeneme/main.java
@@ -0,0 +1,26 @@
+package ilkdeneme;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.print("hello word");
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ilkdeneme/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author cansu
+ *
+ */
+module ilkdeneme {
+}
